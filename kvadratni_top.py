7581ce520e8451442a261d25f765b7d83cb9ff54
Islom Turayev <islomturayev01@gmail.com>
2021-02-23 17:41:20 +0500

    yangi fayl yuklandi

---
+++ kvadratni_top.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 15 11:04:58 2021
+
+@author: User
+"""
+
+son=int(input("birinchi sonni kiriting"))
+son2=int(input("ikkinchi sonni kiriting"))
+javob=son-son2, son+son2, son*son2, son/son2
+print("javob" + str(javob) + "bo'ladi")
